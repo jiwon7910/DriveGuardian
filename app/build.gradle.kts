b64d39aa112bc79abc8c39243e637a4ab8affaee
aajinlee <leehahj77@gmail.com>
2024-01-05 11:55:30 +0900

    login and start page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.firebase:firebase-firestore:24.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -45,5 +47,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+   // Declare the dependency for the Cloud Firestore library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-firestore")
+    implementation ("com.android.volley:volley:1.2.1")
 
 }
--- app/src/main/java/com/wallaby/driveguardian/MainActivity.java
+++ app/src/main/java/com/wallaby/driveguardian/MainActivity.java
@@ -1,94 +1,51 @@
 package com.wallaby.driveguardian;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // onCreate() 메서드에서 FirebaseAuth 인스턴스를 초기화
-        auth = FirebaseAuth.getInstance();
+        Button btn_logout = findViewById(R.id.btn_logout);
+        //로그아웃 버튼
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        // 회원가입 기능
-        Button joinBtn = findViewById(R.id.joinBtn);
-        // 사용자가 이메일과 비밀번호를 입력하고 회원가입 버튼을 누를 경우
-        joinBtn.setOnClickListener(v -> {
-            EditText emailEditText = findViewById(R.id.email);
-            EditText passwordEditText = findViewById(R.id.password);
+                FirebaseAuth.getInstance().signOut();
+                finish();
 
-            auth.createUserWithEmailAndPassword(emailEditText.getText().toString(),
-                            passwordEditText.getText().toString())
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(this, "성공", Toast.LENGTH_LONG).show();
-                        } else {
-                            Toast.makeText(this, "실패", Toast.LENGTH_LONG).show();
-                        }
-                    });
+                //Intent를 새로 만든 후 LoginActivity로 화면전환
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
         });
 
-        // 로그인 기능
-        Button loginBtn = findViewById(R.id.loginBtn);
-        loginBtn.setOnClickListener(v -> {
-            EditText emailEditText = findViewById(R.id.email);
-            EditText passwordEditText = findViewById(R.id.password);
 
-            auth.signInWithEmailAndPassword(emailEditText.getText().toString(),
-                            passwordEditText.getText().toString())
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show();
-                            navigateToMain();
-                        } else {
-                            Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show();
-                        }
-                    });
-        });
-
-        // 비회원 로그인 기능
-        Button noLoginBtn = findViewById(R.id.noLoginBtn);
-        noLoginBtn.setOnClickListener(v -> {
-            auth.signInAnonymously()
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = auth.getCurrentUser();
-                            Log.d("MainActivity", user != null ? user.getUid() : null);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        });
-
-        // 로그아웃 기능
-        Button logoutBtn = findViewById(R.id.logoutBtn);
-        logoutBtn.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            Toast.makeText(this, "로그아웃", Toast.LENGTH_LONG).show();
-        });
     }
-    private void navigateToMain() {
-        Intent intent = new Intent(this, Main.class);
-        startActivity(intent);
-        finish(); // 현재 액티비티를 종료하고 Main으로 이동
-    }
+
 
 }
