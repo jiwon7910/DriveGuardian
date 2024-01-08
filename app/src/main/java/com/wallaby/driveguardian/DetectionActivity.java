b0fa2070cb0547f41a1e11d07af64cc13867673f
aajinlee <leehahj77@gmail.com>
2024-01-08 14:28:44 +0900

    mlkit landmark detection with camera (realtime)

--- app/src/main/java/com/wallaby/driveguardian/DetectionActivity.java
+++ app/src/main/java/com/wallaby/driveguardian/DetectionActivity.java
@@ -1,18 +1,23 @@
 package com.wallaby.driveguardian;
 
 import android.Manifest;
-import android.content.ContentValues;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.video.Recorder;
@@ -21,11 +26,19 @@ import androidx.camera.video.VideoCapture;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
 import com.wallaby.driveguardian.databinding.ActivityDetectionBinding;
-import com.wallaby.driveguardian.databinding.ActivityMainBinding;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -36,11 +49,14 @@ public class DetectionActivity extends AppCompatActivity{
     private VideoCapture<Recorder> videoCapture;
     private Recording recording;
 
-    private ExecutorService cameraExecutor;
+    private ExecutorService cameraExecutor; // 백그라운드 스레드에서 카메라 작업을 실행하는데 사용됨
 
-    private static final String TAG = "CameraXApp";
-    private static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
-    private static final int REQUEST_CODE_PERMISSIONS = 10;
+    private GraphicOverlay mGraphicOverlay;
+    private Bitmap mSelectedImage;
+
+    private static final String TAG = "GuardianDrive"; //  Log에 사용되는 태그
+    private static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"; // 비디오 파일 이름 형식을 지정하는 상수
+    private static final int REQUEST_CODE_PERMISSIONS = 10; //권한을 요청할 때 사용되는 요청 코드
 
     private static final String[] REQUIRED_PERMISSIONS = {
             Manifest.permission.CAMERA,
@@ -64,16 +80,52 @@ public class DetectionActivity extends AppCompatActivity{
         cameraExecutor = Executors.newSingleThreadExecutor();
     }
     private void startCamera() {
-        ProcessCameraProvider.getInstance(this).addListener(() -> {
+        // camera 호출
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
             try {
                 // Used to bind the lifecycle of cameras to the lifecycle owner
-                ProcessCameraProvider cameraProvider = ProcessCameraProvider.getInstance(this).get();
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
 
                 // Preview
                 Preview preview = new Preview.Builder().build();
                 preview.setSurfaceProvider(viewBinding.viewFinder.getSurfaceProvider());
 
-                imageCapture = new ImageCapture.Builder().build();
+                // ImageAnalysis
+                ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+                imageAnalysis.setAnalyzer(cameraExecutor, imageProxy -> {
+                    // Access the ByteBuffer of the image
+                    ByteBuffer buffer = imageProxy.getPlanes()[0].getBuffer();
+                    byte[] data = new byte[buffer.remaining()];
+                    buffer.get(data);
+
+                    // Get the image dimensions
+                    int width = imageProxy.getWidth();
+                    int height = imageProxy.getHeight();
+
+                    // Create a YuvImage from the image data
+                    YuvImage yuvImage = new YuvImage(data, ImageFormat.NV21, width, height, null);
+
+                    // Convert YuvImage to JPEG
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    yuvImage.compressToJpeg(new Rect(0, 0, width, height), 100, out);
+
+                    // Convert JPEG to Bitmap
+                    byte[] jpegBytes = out.toByteArray();
+                    Bitmap imageBitmap = BitmapFactory.decodeByteArray(jpegBytes, 0, jpegBytes.length);
+
+                    // Update mSelectedImage with the current image
+                    mSelectedImage = imageBitmap;
+
+                    // Close the imageProxy to release resources
+                    imageProxy.close();
+
+                    runFaceContourDetection();
+                });
 
                 // Select back camera as a default
                 CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
@@ -82,14 +134,13 @@ public class DetectionActivity extends AppCompatActivity{
                 cameraProvider.unbindAll();
 
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+                Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis);
 
             } catch (Exception exc) {
                 Log.e(TAG, "Use case binding failed", exc);
-            }
+        }
         }, ContextCompat.getMainExecutor(this));
     }
-
     private boolean allPermissionsGranted() {
         for (String permission : REQUIRED_PERMISSIONS) {
             if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
@@ -119,4 +170,44 @@ public class DetectionActivity extends AppCompatActivity{
             }
         }
     }
+
+
+    private void runFaceContourDetection() {
+        // Replace with code from the codelab to run face contour detection.
+        InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
+        FaceDetectorOptions options =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+                        .build();
+
+        FaceDetector detector = FaceDetection.getClient(options);
+        // Task failed with an exception
+        detector.process(image)
+                .addOnSuccessListener(
+                        this::processFaceContourDetectionResult)
+                .addOnFailureListener(
+                        Throwable::printStackTrace);
+    }
+
+    private void processFaceContourDetectionResult(List<Face> faces) {
+        // Replace with code from the codelab to process the face contour detection result.
+        // Task completed successfully
+        if (faces.size() == 0) {
+            showToast("No face found");
+            return;
+        }
+        mGraphicOverlay.clear();
+        for (int i = 0; i < faces.size(); ++i) {
+            Face face = faces.get(i);
+            FaceContourGraphic faceGraphic = new FaceContourGraphic(mGraphicOverlay);
+            mGraphicOverlay.add(faceGraphic);
+            faceGraphic.updateFace(face);
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
 }
